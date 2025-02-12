62db1c7095a1c86d5ac2b6ce9da3817079b777db
LocDayNha <vohoangloc200@gmail.com>
2025-02-12 08:34:58 +0700

    add item

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Login from "../../src/components/ui/begin/Login";
 import Home from "../../src/components/ui/main/Home";
-import { CustomTextInput, CustomButton } from "../../src/components/item/Item";
+import { TextInputBegin, TextInputMain, CustomButton, TextInputProfile } from "../../src/components/item/Item";
 
 export default function App() {
 
@@ -11,12 +11,7 @@ export default function App() {
 
   return (
     <SafeAreaView>
-      <CustomTextInput
-        value={text}
-        onChangeText={setText}
-        placeholder="Nhập nội dung..."
-      />
-      <CustomButton label={'Bấm vào đây'} onPress={() => console.log("Button pressed")} />
+      <Login/>
     </SafeAreaView>
   );
 }
--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,11 +1,38 @@
-import { StyleSheet, View, Text, TouchableOpacity, TextInput } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, TextInput, Image } from "react-native";
 import { COLOR } from "../../assets/Theme/Theme";
 
-export function CustomTextInput({ value, onChangeText, placeholder }) {
+export function TextInputBegin({ uri, onChangeText, placeholder }) {
+  return (
+    <View style={styles.inputContainer}>
+      <Image source={uri} style={styles.img} />
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+}
+
+export function TextInputMain({ value, onChangeText, placeholder }) {
   return (
     <View style={styles.inputContainer}>
       <TextInput
         style={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+}
+
+export function TextInputProfile({ label, value, onChangeText, placeholder }) {
+  return (
+    <View style={styles.viewInputProfile}>
+      <Text style={styles.textProfile}>{label}</Text>
+      <TextInput
+        style={styles.inputProfile}
         placeholder={placeholder}
         value={value}
         onChangeText={onChangeText}
@@ -22,32 +49,75 @@ export function CustomButton({ label, onPress }) {
   );
 }
 
+export function ItemRating({data}) {
+  return (
+    <View></View>
+  );
+}
+
+export function ItemStation({data}) {
+  return (
+    <View></View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
-    width: "90%",
+    width: "85%",
     height: 50,
     backgroundColor: COLOR.primary,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 30,
+    borderRadius: 25,
   },
   buttonText: {
-    color: "black",
-    fontSize: 16,
-    color: "#fff"
+    fontSize: 20,
+    color: COLOR.secondary,
+    fontWeight: 700,
+    fontFamily: 'Poppins'
   },
   inputContainer: {
-    width: "90%",
-    padding: 10,
+    width: "85%",
+    height: 50,
+    paddingLeft: 10,
     borderWidth: 1,
-    borderColor: COLOR.primary,
+    borderColor: COLOR.gray2,
     borderRadius: 10,
     backgroundColor: "#fff",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   input: {
     width: "100%",
-    height: 40,
+    height: "100%",
     fontSize: 16,
     paddingHorizontal: 10,
   },
+  img: {
+    marginLeft: "5%"
+  },
+  viewInputProfile: {
+    width: "85%",
+    height: "25%",
+    paddingLeft: 10,
+    borderColor: COLOR.gray2,
+    borderRadius: 10,
+  },
+  textProfile: {
+    fontSize: 20,
+    color: COLOR.black,
+    fontWeight: 700,
+    fontFamily: 'Poppins'
+  },
+  inputProfile: {
+    width: "100%",
+    height: 50,
+    fontSize: 16,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: COLOR.gray,
+    borderRadius: 15
+  }
+
 });
